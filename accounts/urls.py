ca20f3fc77320731d20a952a84f296b196548897
maeve <mujammilkhan321@gmail.com>
2020-08-12 19:01:59 +0530

    bugs fixed

--- accounts/urls.py
+++ accounts/urls.py
@@ -9,7 +9,7 @@ from knox.views import LogoutView
 urlpatterns = [
     path("", get_user_view),
     path("register/", register_view),
-    path("logi/n", login_view),
+    path("login/", login_view),
     path("logout/", LogoutView.as_view(), name="LogoutView"),
     path("confirmregistration/", confirm_registration_view),
     path("resetpassword/getusername/", create_password_reset_token_view),
