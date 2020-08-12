cfc2e77cac3410784b529080633968e8371f1b58
maeve <mujammilkhan321@gmail.com>
2020-08-12 17:35:08 +0530

    .

--- epubreader/settings.py
+++ epubreader/settings.py
@@ -93,9 +93,9 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-DATABASES['default'] = dj_database_url.config(
-    default=os.environ.get('DATABASE_URL')
-)
+# DATABASES['default'] = dj_database_url.config(
+#     default=os.environ.get('DATABASE_URL')
+# )
 
 # DATABASES['default'] = dj_database_url.config(
 #     conn_max_age=600, ssl_require=True)
